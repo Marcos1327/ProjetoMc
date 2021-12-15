e8c659733a1589588749306fab13c200ace63a7e
Marcos Giovanny <giovannygigi@gmail.com>
2021-12-15 15:41:36 -0300

    Project Created

---
+++ src/main/java/com/marcos/projetosts/ProjetostsApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.projetosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/projetosts/ProjetostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.projetosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
