4f07715822d29371f5bd327babe38f862b235c4e
Marcos Giovanny <giovannygigi@gmail.com>
2021-12-15 16:21:08 -0300

    Classe domain criada e testada

--- src/main/java/com/marcos/projetosts/resource/CategoriaResource.java
+++ src/main/java/com/marcos/projetosts/resource/CategoriaResource.java
@@ -1,17 +1,29 @@
 package com.marcos.projetosts.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcos.projetosts.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Rest está funcionando";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> list() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 
-	
 }
