bdebaea8f52da7c06cd30ab3dac35460fcc83944
Marcos Giovanny <giovannygigi@gmail.com>
2022-01-05 15:04:22 -0300

    Criando operacao de Instaciacao

--- src/main/java/com/marcos/projetosts/ProjetostsApplication.java
+++ src/main/java/com/marcos/projetosts/ProjetostsApplication.java
@@ -1,13 +1,34 @@
 package com.marcos.projetosts;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marcos.projetosts.domain.Categoria;
+import com.marcos.projetosts.domain.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetostsApplication {
+public class ProjetostsApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetostsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
