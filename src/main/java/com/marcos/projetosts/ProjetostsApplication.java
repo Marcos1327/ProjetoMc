b6a94fd78a23111e9408d6decb9f061e27948778
Marcos Giovanny <giovannygigi@gmail.com>
2022-01-19 15:55:32 -0300

    Paginacao com paramentos opcionais na requisicao

--- src/main/java/com/marcos/projetosts/ProjetostsApplication.java
+++ src/main/java/com/marcos/projetosts/ProjetostsApplication.java
@@ -66,6 +66,12 @@ public class ProjetostsApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Came mesa e banho");
+		Categoria cat4 = new Categoria(null, "Roupas");
+		Categoria cat5 = new Categoria(null, "Peça de carro");
+		Categoria cat6 = new Categoria(null, "Casa");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		
 		
 		Product p1 = new Product(null, "Computador", 2000.00);
 		Product p2 = new Product(null, "Impressora", 800.00);
@@ -78,7 +84,7 @@ public class ProjetostsApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));		
 				
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/marcos/projetosts/resource/CategoriaResource.java
+++ src/main/java/com/marcos/projetosts/resource/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -64,6 +66,8 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	// Método para listar apenas a categoria sem os produtos, no banco de dados / junto com o DTO
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list= service.findAll();
@@ -73,5 +77,17 @@ public class CategoriaResource {
 	
 	}
 	
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "name")String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+		Page<Categoria> list= service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
+	}
 
 }
