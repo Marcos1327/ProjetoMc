f641a5a6aad0e2ee63d305fa4b9baadc7cbaefa2
Marcos Giovanny <giovannygigi@gmail.com>
2022-02-02 13:30:55 -0300

    PUT DELETE e GET para cliente

--- src/main/java/com/marcos/projetosts/domain/services/ClienteService.java
+++ src/main/java/com/marcos/projetosts/domain/services/ClienteService.java
@@ -1,13 +1,21 @@
 package com.marcos.projetosts.domain.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcos.projetosts.domain.Cliente;
+import com.marcos.projetosts.domain.Cliente;
 import com.marcos.projetosts.domain.repositories.ClienteRepository;
+import com.marcos.projetosts.domain.services.exceptions.DataIntegrityException;
 import com.marcos.projetosts.domain.services.exceptions.ObjectNotFoundException;
+import com.marcos.projetosts.dto.ClienteDTO;
 
 @Service
 public class ClienteService {
@@ -20,6 +28,45 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
+	}
+	
+	// Método para Atualizar no banco de dados / junto com o Resource
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	// Método para Delete no banco de dados / junto com o Resource
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+				repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+		}
+		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
--- src/main/java/com/marcos/projetosts/resource/ClienteResource.java
+++ src/main/java/com/marcos/projetosts/resource/ClienteResource.java
@@ -1,14 +1,23 @@
 package com.marcos.projetosts.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcos.projetosts.domain.Cliente;
 import com.marcos.projetosts.domain.services.ClienteService;
+import com.marcos.projetosts.dto.ClienteDTO;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -26,4 +35,50 @@ public class ClienteResource {
 	
 	}
 
+	
+	
+	// Método para Atualizar no banco de dados / junto com o Service
+	
+		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id ){
+			Cliente obj = service.fromDTO(objDto);
+			obj.setId(id);
+			obj = service.update(obj);
+			return ResponseEntity.noContent().build();
+			
+			
+		}
+		
+		// Método para Delete no banco de dados / junto com o Service
+		
+		@RequestMapping(value = "/{id}" ,method=RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Integer id) {
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
+		
+		// Método para listar apenas a categoria sem os produtos, no banco de dados / junto com o DTO
+		
+		@RequestMapping(method = RequestMethod.GET)
+		public ResponseEntity<List<ClienteDTO>> findAll() {
+			List<Cliente> list= service.findAll();
+			List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+			
+			return ResponseEntity.ok().body(listDto);
+		
+		}
+		
+		//Método para a ajustar a página pelos parametos listados
+		
+		@RequestMapping(value="/page", method = RequestMethod.GET)
+		public ResponseEntity<Page<ClienteDTO>> findPage(
+				@RequestParam(value="page", defaultValue = "0") Integer page,
+				@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage,
+				@RequestParam(value="orderBy", defaultValue = "name")String orderBy,
+				@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+			Page<Cliente> list= service.findPage(page, linesPerPage, orderBy, direction);
+			Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+			
+			return ResponseEntity.ok().body(listDto);
+		}
 }
